33de77a4e238dd4b481c76642966f3388a5e6afc
Javier <notri80@gmail.com>
2023-05-23 23:17:04 +0200

    Draw food(apples) randomly on the panel board

--- src/snakeGame/GameFrame.java
+++ src/snakeGame/GameFrame.java
@@ -5,8 +5,8 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame{
     //vars
     GamePanel panel = new GamePanel();
-    int width = 400;
-    int height = 500;
+    int width = 640;
+    int height = 635;
     GameFrame(){
         this.add(panel);
        this.setSize(width, height);
--- src/snakeGame/GamePanel.java
+++ src/snakeGame/GamePanel.java
@@ -6,24 +6,73 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
     
+    static final int SCREEN_WIDTH = 640;
+    static final int SCREEN_HEIGHT = 638;
+    // ech item is going to have a dimension of 25px(for wight & height)
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
     GamePanel (){
-        
+        random = new Random();
+       this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+       this.setBackground(Color.black);
+       this.setFocusable(true);
+       this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
+
     public void startGame(){
+         newApple();
+         running = true;
+      // DELAY dictates how fast the game runs, 
+      // and we pass in "this" because we're using the action listener interface
+         timer = new Timer(DELAY, this);
+         timer.start();
+    }
+    
+    public void newApple(){     // cast it as an integer to not break the program
+        appleX = random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
+        appleY = random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
 
     }
 
     public void paintComponent(Graphics g){
+        // This method will apply to panel the settings we set before Color, size, fonts..
+        super.paintComponent(g);
+        draw(g);
         
     }
-
+    
+    /**
+     * 
+     * This method takes care of drawing everything on the panel
+     */
     public void draw(Graphics g){
-
+      // THIS DRAWS A GRID IN AOUR PANEL
+        for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+           //  drawLine(int x1, int y1,       int x2, int y2)
+                    //STARTING COORDANATES //ENDING COORDINATES
+             g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+             g.drawLine(0, i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
 
     public void move(){
-
+     
     }
 
     public void feedSnake(){
